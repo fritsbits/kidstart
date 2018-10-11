38c04e106baa16342484de40e4e72819ab6b8d70
Frederik Vincx <frederik.vincx@gmail.com>
2018-10-11 10:53:14 +0200

    Add initial laravel project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,187 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Kidstart – Breng specialisten in de klas</title>
+
+    <!-- Styles -->
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.typekit.net/pdy3trh.css">
+    <link rel="stylesheet" href="https://use.typekit.net/pdy3trh.css">
+
+</head>
+<body>
+
+    <section class="header">
+      <img src="/img/kidstart-logo.svg" alt="" class="logo">
+      <h1>Breng specialisten in de klas</h1>
+  </section>
+
+
+  <section class="intro container-fluid">
+
+    <div class="row">
+        <div class="col-md-4 pl-0 d-none d-md-block d-lg-block d-xl-block">
+            <img src="/img/homepage/rocketkid.jpg" alt="" class="img-rocketkid img-fluid">
+        </div>
+        <div class="col-md-6">
+            <p class="lead">Jongeren kiezen in hun schoolcarrière veel te zelden een  studierichting op basis van hun interesses. Hoe kunnen we hen inspireren om hun passies vroeg te ontdekken?</p>
+
+
+            <img src="/img/homepage/rocketkid.jpg" alt="" class="img-rocketkid img-fluid d-md-none d-lg-none d-xl-none">
+
+
+            <p>Er is een Afrikaans spreekwoord dat zegt: </p>
+
+
+            <blockquote><p>It takes a community to raise a child.</p></blockquote>
+
+            <p>Daar ligt de sleutel tot succes. In een gedeelde verantwoordelijkheid tussen school, ouders, buurt, verenigingen en bedrijven.</p>
+
+            <p>Dat toekomstbeeld willen wij waarmaken met Kidstart.</p>
+        </div>
+        
+    </div>
+    
+</section>
+
+
+<section class="product container">
+
+    <h2 class="text-center">Haal specialisten in de klas</h2>
+    <p class="subtitle text-center">Gepassioneerde mensen die de horizon van kinderen verbreden</p>
+
+    <div class="row">
+        <div class="col-lg-8 offset-lg-2">
+            <img src="/img/homepage/screen.png" alt="" class="img-fluid mx-auto">    
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4 offset-md-2">
+            <h3>Matchmaking</h3>
+            <p>Kidstart helpt scholen om inspirende mensen in de klas te krijgen. Via de website matchen we scholen met gepassioneerde vrijwilligers uit de buurt.</p>
+        </div>
+
+        <div class="col-md-4">
+            <h3>Begeleiding</h3>
+            <p>Leerkrachten en gastdocenten krijgen met Kidstart ook toolkits om snel goede gastlessen te geven.  </p>
+        </div>
+    </div>
+    
+</section>
+
+
+<img src="/img/homepage/paviljoenbollen.jpg" class="img-fluid d-md-none d-lg-none d-xl-none mb-5" alt="Paviljoen Schaarbeek">
+
+<section class="pilot container-fluid">
+
+    <div class="row">
+        <div class="col-md-7 col-lg-5 offset-lg-1">
+            <h2>Piloot project in Schaarbeek</h2>
+            <p class="subtitle">Stap voor stap richting een open school</p>
+
+            <p>Kidstart is nog in de beginfase. Momenteel doorlopen we een piloottraject met lagere school  <a href="http://paviljoen-schaarbeek.be" target="_blank">Paviljoen Schaarbeek</a> en worden we mee ondersteund door de directeur Sven Moens, die ook het <a href="http://tada.brussels" target="_blank">ToekomstAtelierDelavenir</a> ondersteunde.</p>
+
+            <h3>Geïnteresseerd als gastdocent?</h3>
+            <p>Jeukt het al om jouw passie te delen met de ketjes van Paviljoen? We horen er graag meer over, om te kijken hoe je een namiddag mee kan helpen in de klas.</p>
+
+            <a href="https://airtable.com/shr1PN0ILwXSRx4W5" target="_blank" class="btn btn-primary">Deel je passie</a>
+
+        </div>
+
+        <div class="col-md-5 col-lg-6 pr-0 text-right d-none d-sm-none d-md-block d-lg-block d-xl-block">
+            <img src="/img/homepage/paviljoenbollen.jpg" class="img-fluid" alt="">
+        </div>
+    </div>
+    
+</section>
+
+
+<section class="news container-fluid">
+
+    <div class="row">
+        <div class="col-md-4 pl-0">
+            <img src="/img/homepage/newsbollen.svg" alt="" class="img-fluid">
+        </div>
+        <div class="col-md-6">
+
+            <h2>Updates</h2>
+            <p><a href="https://medium.com/kidstart/kidstart-en-basisschool-paviljoen-schaarbeek-samen-uit-de-startblokken-1f8e5dedbe20" target="_blank">Kidstart en Basisschool Paviljoen Schaarbeek samen uit de startblokken</a></p>
+        </div>
+        
+    </div>
+    
+</section>
+
+
+<section class="team">
+
+    <img src="/img/homepage/hackbelgium.jpg" alt="Kidstart op Hack Belgium" class="w-100">
+    
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-8 col-lg-4">
+                <h2>Team Kidstart</h2>
+                <p class="mb-5">Kidstart ontstond op <a href="http://www.hackbelgium.be" target="_blank">Hack Belgium</a>, de grootste hackathon van België, waar ons project laureaat was. Oprichters Ilse en Pieter-Jan bedachtten er het project. Frederik kwam er bij nadat hij <a href="https://www.youtube.com/watch?v=4Ql0QwyUx5g&feature=youtu.be&t=25" target="_blank">hun pitch</a> zag op hack Belgium.</p>
+            </div>
+
+            <div class="col-lg-8">
+                <div class="row">
+                    <div class="col-md-4 team-member">
+                        <img src="/img/homepage/profile-pieterjan.jpg" alt="Pieter-Jan Herman" class="img-fluid">
+                        <h4><a href="https://www.linkedin.com/in/pieterjanherman/" target="_blank">Pieter-Jan Herman</a></h4>
+                        <p class="team-member-function">Leerkracht middelbaar onderwijs</p>
+                    </div>
+                    <div class="col-md-4 team-member">
+                        <img src="/img/homepage/profile-ilse.jpg" alt="Ilse Taildeman" class="img-fluid">
+                        <h4><a href="https://www.linkedin.com/in/ilse-taildeman-5b990448/" target="_blank">Ilse Taildeman</a></h4>
+                        <p class="team-member-function">Projectmanager duurzame ontwikkeling</p>
+                    </div>
+                    <div class="col-md-4 team-member">
+                        <img src="/img/homepage/profile-frederik.jpg" alt="Pieter-Jan Herman" class="img-fluid">
+                        <h4><a href="https://www.linkedin.com/in/frederikvincx/" target="_blank">Frederik Vincx</a></h4>
+                        <p class="team-member-function">Social service designer</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+
+<section class="newsletter container-fluid d-none">
+
+
+    <div class="row">
+        <div class="col-md-4 pl-0 text-right">
+            <img src="/img/homepage/logocircle.png" alt="" class="img-fluid img-logocircle">
+        </div>
+        <div class="col-md-6">
+           <h2>Blijf op de hoogte</h2>
+           <p class="subtitle">Weet als eerste wanneer we in nieuwe scholen lanceren</p>
+
+       </div>
+
+   </div>
+
+
+
+
+</section>
+
+<footer class="footer d-print-none">
+
+    <img src="/img/kidstart-logo.svg" alt="" class="logo">
+    <p>It takes a community to raise a child</p>
+
+</footer>
+
+
+</body>
+</html>
