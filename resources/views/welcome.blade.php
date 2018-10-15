cf36451b51f8eb34ba48bb91f67eb6151fd1ece7
Frederik Vincx <frederik.vincx@gmail.com>
2018-10-15 19:15:16 +0200

    Add favicons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,28 @@
     <link rel="stylesheet" href="https://use.typekit.net/pdy3trh.css">
     <link rel="stylesheet" href="https://use.typekit.net/pdy3trh.css">
 
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="{{ asset('apple-touch-icon-57x57.png') }}" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ asset('apple-touch-icon-114x114.png') }}" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset('apple-touch-icon-72x72.png') }}" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ asset('apple-touch-icon-144x144.png') }}" />
+    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="{{ asset('apple-touch-icon-60x60.png') }}" />
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="{{ asset('apple-touch-icon-120x120.png') }}" />
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="{{ asset('apple-touch-icon-76x76.png') }}" />
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="{{ asset('apple-touch-icon-152x152.png') }}" />
+    <link rel="icon" type="image/png" href="{{ asset('favicon-196x196.png" sizes="196x196') }}" />
+    <link rel="icon" type="image/png" href="{{ asset('favicon-96x96.png" sizes="96x96') }}" />
+    <link rel="icon" type="image/png" href="{{ asset('favicon-32x32.png" sizes="32x32') }}" />
+    <link rel="icon" type="image/png" href="{{ asset('favicon-16x16.png" sizes="16x16') }}" />
+    <link rel="icon" type="image/png" href="{{ asset('favicon-128.png" sizes="128x128') }}" />
+    <meta name="application-name" content="&nbsp;"/>
+    <meta name="msapplication-TileColor" content="#FFFFFF" />
+    <meta name="msapplication-TileImage" content="{{ asset('mstile-144x144.png') }}" />
+    <meta name="msapplication-square70x70logo" content="{{ asset('mstile-70x70.png') }}" />
+    <meta name="msapplication-square150x150logo" content="{{ asset('mstile-150x150.png') }}" />
+    <meta name="msapplication-wide310x150logo" content="{{ asset('mstile-310x150.png') }}" />
+    <meta name="msapplication-square310x310logo" content="{{ asset('mstile-310x310.png') }}" />
+
+
 </head>
 <body>
 
@@ -163,12 +185,12 @@
             <img src="/img/homepage/logocircle.png" alt="" class="img-fluid img-logocircle">
         </div>
         <div class="col-md-6">
-           <h2>Blijf op de hoogte</h2>
-           <p class="subtitle">Weet als eerste wanneer we in nieuwe scholen lanceren</p>
+         <h2>Blijf op de hoogte</h2>
+         <p class="subtitle">Weet als eerste wanneer we in nieuwe scholen lanceren</p>
 
-       </div>
+     </div>
 
-   </div>
+ </div>
 
 
 
